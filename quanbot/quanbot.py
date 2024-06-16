f0d2ca0aeaebf725e773c78b6708e789752ed8b7
Glazer <140729912+NotGlazer@users.noreply.github.com>
2024-06-17 00:29:44 +0700

    Update quanbot.py

--- quanbot/quanbot.py
+++ quanbot/quanbot.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 
 client = OpenAI(
-    # This is the default and can be omitted
+    # Input api key store trong tài khoản streamlit để bảo mật và tránh bị cướp key
     api_key=st.secrets["apikey"],
 )
 
@@ -15,44 +15,47 @@ def chat_with_gpt(userprompt):
     now = datetime.now()
     current_date = now.strftime("hôm nay là ngày %d tháng %m năm %Y")
 
-    # Define the prerequisite prompt
+    # system prompt để điều chỉnh bot phù hợp
     prerequisite_prompt = f"Tên bạn là Lê Nhật Quân, một bot thân thiện, cởi mở và tử tế chỉ có thể trả lời bằng tiếng Việt, tuyệt đối không được sử dụng ngôn ngữ nào khác. {current_date}."
 
-    # Create the messages list with the prerequisite prompt
+    # tạo array messages với system prompt và prompt người dùng
     messages = [
         {"role": "system", "content": prerequisite_prompt},
         {"role": "user", "content": userprompt}
     ]
 
+    #tạo chat_completion sau khi hoàn thành input prompt
     chat_completion = client.chat.completions.create(
         messages=messages,
         model="gpt-3.5-turbo",
         temperature=0.3
     )
 
+    #return câu trả lời của bot
     return chat_completion.choices[0].message.content.strip()
 
-# Initialize chat history
+#UI sử dụng streamlit
+# Tạo session để store các câu hỏi của người dùng và chat của bot
 if "responses" not in st.session_state:
     st.session_state["responses"] = []
 
-# Display existing conversations
+# Tạo một UI display hết message của bot và người dùng
 for message in st.session_state["responses"]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Input interface below the chatbox
+# Tạo một box để người dùng nhập chữ chat với bot
 if prompt := st.chat_input("Chat với bot Quân"):
-    # Display user message in chat message container
+    # display message trong UI chatbox
     st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
+    # thêm message vào session
     st.session_state["responses"].append({"role": "user", "content": prompt})
 
-    # Get response from ChatGPT
+    # Lấy câu trả lời của bot bằng cách truy xuất biến prompt (thông tin người dùng nhập) vào function chat_with_gpt trên
     response = chat_with_gpt(prompt)
     
-    # Display assistant response in chat message container
+    # display message câu trả lời của bot trong UI chatbox
     with st.chat_message("assistant"):
         st.markdown(response)
-    # Add assistant response to chat history
+    # thêm message vào session
     st.session_state["responses"].append({"role": "assistant", "content": response})
